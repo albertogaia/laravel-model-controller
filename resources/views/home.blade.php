fcb55452072a5e3fc868881e50fd3e1ac78f34af
albertogaia <alberto.gaia95@gmail.com>
2021-10-28 14:47:44 +0200

    Creata app con controller e model

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title', 'Homepage')
+    
+@section('content')
+    <h1>Sono il main</h1>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,3 @@
+<header>
+    <h2>Sono l'Header</h2>
+</header>
